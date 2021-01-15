aa867a27ff1e9d53809ea3c5643f92b5cd7b8b9a
Your Username <you@example.com>
2021-01-15 12:00:47 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -4,4 +4,8 @@ require 'interactive_record.rb'
 
 class Student < InteractiveRecord
 
+    column_names.each do |attribute| 
+      attr_accessor  attribute.to_sym  
+      #self.attr_accessor(attribute.to_sym)
+    end 
 end
